4ff013b05fda4c66876b50188876491dd605a425
farhan ahmad <70254448+farhanahmad322@users.noreply.github.com>
2023-10-30 10:42:24 +0000

    const let var

---
+++ 01_besic/01_variales.js
@@ -0,0 +1,16 @@
+const accountId=123456;
+let accountEmail="ahmadfar322@gail.com";
+var accountPassword="13438";
+accountCity="jaipur";
+let accountState;
+
+// accountId=2;  not allowed
+accountEmail="farray96@gmail.com"
+accountPassword="67774";
+accountCity="gaya";
+console.log(accountId);
+/*
+prefer not to use var becouse of issue in block scope and func scope
+*/
+
+console.table([accountId, accountEmail,accountPassword,accountCity,accountState]);
