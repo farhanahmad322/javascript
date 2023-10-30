e554f4463ba280f7db1f7efea281d5cfaba7585f
farhan ahmad <70254448+farhanahmad322@users.noreply.github.com>
2023-10-30 11:07:41 +0000

    datatype

---
+++ 01_besic/02_datatype.js
@@ -0,0 +1,26 @@
+"use strict";  // treat all JS code as newer version
+
+// alert( 3 + 3)  // we are using nodejs, not browser
+
+console.log(3 +
+    3)
+
+console.log("farhan")
+
+let name = "farhan"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // O/P- undefined
+console.log(typeof null);  // O/P- object
