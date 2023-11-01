3c84da051be5e96059c2f176b8925e451ec3de93
farhan ahmad <70254448+farhanahmad322@users.noreply.github.com>
2023-11-01 11:39:27 +0000

    stack heap

--- 01_besic/05_datatype-summary.js
+++ 01_besic/05_datatype-summary.js
@@ -38,3 +38,32 @@ const myFunction = function(){
 console.log(typeof myObj);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+// ++++++++++++++++++++++++++++
+
+// 1] stack=> (primitive hot hai)=(copy milta hai variabale ka)
+
+//2] heap(non-primitive hota hai)=([refrence milega] matlb orignal value me chenge hoga)
+
+// let myname1="farhan";
+// let anothername=myname1;
+
+// anothername="saubi"
+
+// console.log(myname1);
+// console.log(anothername);
+
+let userOne={
+    email:"user@gmail",
+    upi:"user@ybl"
+  
+}
+
+let userTwo=userOne
+userTwo.email="farhan@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+
+
